f22fefeafaa5e58f0f9188da1cad486f59378775
Cynthiamor <kabita262011@hotmail.com>
2024-04-17 16:23:56 -0500

    fin del hito 5

--- obj/Debug/net8.0/GuessTheNumber.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuessTheNumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a210d96733d5a8ddc167f416d3ec77563dd6ccd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a5894b6a369d83361a48a934e10b9ec99318b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
