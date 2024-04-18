ea3b92ceb2b5a3f1cb4ecfd67ae5f1fd76b6164f
Cynthiamor <kabita262011@hotmail.com>
2024-04-18 00:25:42 -0500

    actualiza

--- obj/Debug/net8.0/GuessTheNumber.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuessTheNumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+becd2d3c5469939f2bc3e40ff97984af24033eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eda8605796af1152994019f7380f83aa752a40e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
